3967721181afd191f001771136f2a71db0802b74
MrMaxNaJa <111119697+NaJaxHub@users.noreply.github.com>
2025-03-29 13:37:29 +0700

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,3 +1,8 @@
-local whitelistPak {
-  
+-- test.lua
+local whitelistPak = {
+    { Hwid = "HWID123456", Key = "KEY_ABC" },
+    { Hwid = "HWID987654", Key = "KEY_XYZ" },
+    -- บรรทัดนี้จะถูกเพิ่มโดยบอท
 }
+
+-- ฟังก์ชันอื่น ๆ ที่ใช้ในสคริปต์นี้
