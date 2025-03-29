e7ac15b78840caac041c5c3f64b8575339da3593
MrMaxNaJa <111119697+NaJaxHub@users.noreply.github.com>
2025-03-29 13:44:37 +0700

    Add new HWID via Discord Bot

--- test.lua
+++ test.lua
@@ -1,6 +1,8 @@
 -- test.lua
 local whitelistPak = {
-    { Hwid = "123", Key = "lol" }
+    { Hwid = "123", Key = "lol",
+    { Hwid = "123", Key = "lol" },
+}
 }
 
 -- ฟังก์ชันอื่น ๆ ที่ใช้ในสคริปต์นี้
