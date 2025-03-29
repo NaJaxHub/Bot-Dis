f6a3f7c46650795c53e41780fe5eaa9987024287
MrMaxNaJa <111119697+NaJaxHub@users.noreply.github.com>
2025-03-29 13:56:19 +0700

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,8 +1,7 @@
 -- test.lua
 local whitelistPak = {
-    { Hwid = "keysdf5", Key = "hwidaskd",
+    { Hwid = "keysdf5", Key = "hwidaskd"},
     { Hwid = "f", Key = "g" },
-},
 }
 
 -- ฟังก์ชันอื่น ๆ ที่ใช้ในสคริปต์นี้
