862c0f8176d1058d37f6f12a46c9a852dd83f55b
MrMaxNaJa <111119697+NaJaxHub@users.noreply.github.com>
2025-04-21 09:19:10 +0700

    Update Wx.lua

--- Wx.lua
+++ Wx.lua
@@ -1,3 +1,3 @@
 local whitelistPak = {
-    { Hwid = "a", Key = "893294010762928149", ExpiryDate = math.huge, Permanent = true },
+    { Hwid = "", Key = "893294010762928149", ExpiryDate = math.huge, Permanent = true },
 }
