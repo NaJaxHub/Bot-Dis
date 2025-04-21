c61c3a7410bebafdee61a39aa10b6d0128c05b2b
MrMaxNaJa <111119697+NaJaxHub@users.noreply.github.com>
2025-04-21 08:43:51 +0700

    Update HWID data

--- Wx.lua
+++ Wx.lua
@@ -1,5 +1,6 @@
 local whitelistPak = {
     { Hwid = "AMASKMKZX", Key = "893294010762928149" },
+    { Hwid = "AMASKMKZX", Key = "893294010762928149" },
     { Hwid = "HWIDMRMAXNAJA", Key = "893294010762928149" },
     { Hwid = "HWIDMRMAXNAJA", Key = "893294010762928149" },
     { Hwid = "s", Key = "893294010762928149" },
