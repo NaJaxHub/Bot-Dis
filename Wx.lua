ac5367e96f4ffa417976724993da605820086be8
MrMaxNaJa <111119697+NaJaxHub@users.noreply.github.com>
2025-04-21 08:43:24 +0700

    Update HWID data

--- Wx.lua
+++ Wx.lua
@@ -1,4 +1,5 @@
 local whitelistPak = {
     { Hwid = "HWIDMRMAXNAJA", Key = "893294010762928149" },
+    { Hwid = "HWIDMRMAXNAJA", Key = "893294010762928149" },
     { Hwid = "s", Key = "893294010762928149" },
 }
