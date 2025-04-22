58e26840a01768df9f8d4ea3064f7ee6b719654d
MrMaxNaJa <111119697+NaJaxHub@users.noreply.github.com>
2025-04-23 00:53:57 +0700

    Update HWID data

--- Wx.lua
+++ Wx.lua
@@ -1,5 +1,5 @@
 local whitelistPak = {
     { Hwid = "x", Key = "1119509280480038972", ExpiryDate = math.huge, Permanent = true },
-    { Hwid = "53FF23FF-8908-4496-8B86-147B02015C9D", Key = "893294010762928149", ExpiryDate = math.huge, Permanent = true },
+    { Hwid = "ssss", Key = "893294010762928149", ExpiryDate = math.huge, Permanent = true },
 }
 return whitelistPak
