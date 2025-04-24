d6c2db56c4dcbdfca4aff8ca794193fd58bbeceb
MrMaxNaJa <111119697+NaJaxHub@users.noreply.github.com>
2025-04-24 18:50:28 +0700

    Update test2.lua

--- test2.lua
+++ test2.lua
@@ -1,1 +1,5 @@
-a
+local whitelistPak = {
+    { Hwid = "x", Key = "1119509280480038972", ExpiryDate = math.huge, Permanent = true },
+    { Hwid = "ssss", Key = "893294010762928149", ExpiryDate = math.huge, Permanent = true },
+}
+return whitelistPak
