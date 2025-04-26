28fa7ba489e340c95fdc953835e17e47dd1f632f
MrMaxNaJa <111119697+NaJaxHub@users.noreply.github.com>
2025-04-26 20:00:37 +0700

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,5 +1,4 @@
 local whitelistPak = {
-    { Hwid = "sd", Key = "893294010762928149", ExpiryDate = math.huge, Permanent = true, BF = true, KL = false },
     
     
     { Hwid = "x", Key = "1119509280480038972", ExpiryDate = math.huge, Permanent = true },
