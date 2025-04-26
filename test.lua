09f0c53ab3687211b6ab3a0a0e77c667c72faf02
MrMaxNaJa <111119697+NaJaxHub@users.noreply.github.com>
2025-04-26 17:57:39 +0700

    Update HWID data

--- test.lua
+++ test.lua
@@ -1,5 +1,5 @@
 local whitelistPak = {
     { Hwid = "x", Key = "1119509280480038972", ExpiryDate = math.huge, Permanent = true },
-    { Hwid = "0", Key = "893294010762928149", ExpiryDate = math.huge, Permanent = true },
+    { Hwid = "mmmmm", Key = "893294010762928149", ExpiryDate = math.huge, Permanent = true },
 }
 return whitelistPak
