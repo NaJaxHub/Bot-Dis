352fe89648c5a592c17f4d7e93693413e280f75e
MrMaxNaJa <111119697+NaJaxHub@users.noreply.github.com>
2025-05-17 20:01:18 +0700

    Update HWID data

--- Wx.lua
+++ Wx.lua
@@ -1,4 +1,5 @@
 local whitelistPak = {
+    { Hwid = "getgenv().Key = "569037757813620765"  loadstring(game:HttpGet('https://raw.githubusercontent.com/xOne2/_dasd234/refs/heads/main/sdada.lua'))()", Key = "569037757813620765" , ExpiryDate = math.huge, Permanent = true },
     { Hwid = "2", Key = "1208277497796632586" , ExpiryDate = math.huge, Permanent = true },
     { Hwid = "aed04be7-1030-4849-a33e-fdcaa6d6ea37", Key = "1334541958357585931" , ExpiryDate = math.huge, Permanent = true },
     { Hwid = "04CFB423-7E11-4716-A1FE-40A998A1FF2B", Key = "875296029510107176" , ExpiryDate = math.huge, Permanent = true },
