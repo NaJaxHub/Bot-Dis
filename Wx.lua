3de219bdfa228b9f313cdc1b1a4fd897200e3b22
MrMaxNaJa <111119697+NaJaxHub@users.noreply.github.com>
2025-06-06 21:19:15 +0700

    Update HWID data

--- Wx.lua
+++ Wx.lua
@@ -4,6 +4,6 @@ local whitelistPak = {
     { Hwid = "aed04be7-1030-4849-a33e-fdcaa6d6ea37", Key = "1334541958357585931" , ExpiryDate = math.huge, Permanent = true },
     { Hwid = "04CFB423-7E11-4716-A1FE-40A998A1FF2B", Key = "875296029510107176" , ExpiryDate = math.huge, Permanent = true },
     { Hwid = "x", Key = "1119509280480038972", ExpiryDate = math.huge, Permanent = true },
-    { Hwid = "ssss", Key = "893294010762928149", ExpiryDate = math.huge, Permanent = true },
+    { Hwid = "00000000000005", Key = "893294010762928149", ExpiryDate = math.huge, Permanent = true },
 }
 return whitelistPak
