2192abcb2ee3947f56a4dc2a5be0f4e1a602a667
MrMaxNaJa <111119697+NaJaxHub@users.noreply.github.com>
2025-07-10 15:01:06 +0700

    🔄 Auto-update whitelist.lua from bot

--- whitelist.lua
+++ whitelist.lua
@@ -1,22 +1,7 @@
 local whitelistPak = {
     {
-        Hwid = "9A71BBB6-FFD5-412D-AC85-A63C1F47FE04",
-        Key = "8932940107629281490",
-        Verify = false
-    },
-    {
-        Hwid = "9A71BBB6-FFD5-412D-AC85-A63C1F47FE04",
-        Key = "89329401076292814900",
-        Verify = false
-    },
-    {
-        Hwid = "9A71BBB6-FFD5-412D-AC85-A63C1F47FE04",
-        Key = "893294010762928149",
-        Verify = true
-    },
-    {
-        Hwid = "9A71BBB6-FFD5-412D-AC85-A63C1F47FE04",
-        Key = "0893294010762928149",
+        Hwid = "xxx",
+        Key = "yyy",
         Verify = true
     },
 }
