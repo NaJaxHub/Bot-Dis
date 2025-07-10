d9ca4862fc2008d54ce74fc205de2738ef994e08
MrMaxNaJa <111119697+NaJaxHub@users.noreply.github.com>
2025-07-10 14:38:32 +0700

    🔄 Auto-update whitelist.lua from bot

--- whitelist.lua
+++ whitelist.lua
@@ -4,5 +4,10 @@ local whitelistPak = {
         Key = "893294010762928149",
         Verify = true
     },
+    {
+        Hwid = "9A71BBB6-FFD5-412D-AC85-A63C1F47FE04",
+        Key = "0893294010762928149",
+        Verify = true
+    },
 }
 return whitelistPak
