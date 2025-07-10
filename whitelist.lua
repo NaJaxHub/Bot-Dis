534512b19ef9482dffbfe9e9184892ae8c86ff0a
MrMaxNaJa <111119697+NaJaxHub@users.noreply.github.com>
2025-07-10 14:37:03 +0700

    🔄 Auto-update whitelist.lua from bot

--- whitelist.lua
+++ whitelist.lua
@@ -1,17 +1,7 @@
 local whitelistPak = {
     {
-        Hwid = "0000000000000000000",
+        Hwid = "9A71BBB6-FFD5-412D-AC85-A63C1F47FE04",
         Key = "893294010762928149",
-        Verify = true
-    },
-    {
-        Hwid = "1-2-3-4-5",
-        Key = "s",
-        Verify = true
-    },
-    {
-        Hwid = "1-2-3-4-5",
-        Key = "sf",
         Verify = true
     },
 }
